6df23e4ccf405faf9ac875a99200b3ae003ef595
sunnyhuang2008 <sunny-20080101@hotmail.com>
2018-01-25 02:42:04 -0500

    added TEV bounds to graph

--- modules/articles/client/controllers/list-articles.client.controller.js
+++ modules/articles/client/controllers/list-articles.client.controller.js
@@ -14,6 +14,7 @@
     $scope.shares = null; //User input weight
     $scope.startDate = null; //Start date for data
     $scope.endDate = null; // End date for data 
+    $scope.impliedVol = null; 
     $scope.portfolio = new Map(); //Portfolio info with tickers and weights
 
 //Chart Stuff------------------------------------------
@@ -27,7 +28,31 @@ $scope.myChart = new Chart(ctx, {
       label: 'Portfolio Value',
       data: [],
       backgroundColor: "rgba(153,255,51,0.4)"
-    }]
+    },{
+      label: 'Realized volatility 1 std up-bound',
+      fill: false,
+      data: [],
+      borderColor: 'black',
+       borderDash: [10,10]
+    },{
+      label: 'Realized volatility 1 std lo-bound',
+      fill: false,
+      data: [],
+      borderColor: 'black',
+       borderDash: [10,10]
+    },{
+      label: 'Realized volatility 2 std lo-bound',
+      fill: false,
+      data: [],
+      borderColor: 'red',
+       borderDash: [10,10]
+    },{
+      label: 'Realized volatility 2 std lo-bound',
+      fill: false,
+      data: [],
+      borderColor: 'red',
+       borderDash: [10,10]
+    },]
   }
 });
 
@@ -117,23 +142,36 @@ $scope.$on('priceLoadComplete', function(){
 		    });
     $scope.cumulatedReturns = function(portfolio){
       var returns = [];
+      var realizedVol = [];
        $scope.myChart.data.labels=[];
       $scope.myChart.data.datasets[0].data=[];
+      $scope.myChart.data.datasets[1].data=[];
+      $scope.myChart.data.datasets[2].data=[];
+      $scope.myChart.data.datasets[3].data=[];
+      $scope.myChart.data.datasets[4].data=[];
       for(var item of portfolio){
         var portfolio_return = {
           'date': item.date,
           'return': 0
         };
         returns.push(portfolio_return);
+        realizedVol.push(0);
       }
 
+      var counter = 1; 
       for(var i = portfolio.length-2; i >=0 ; i--){
         returns[i].return = (portfolio[i].value/portfolio[i+1].value)-1 + returns[i+1].return; 
+        realizedVol[i] = ($scope.impliedVol)*Math.sqrt(counter)/Math.sqrt(252);
+        counter++;
       }
 
       for(var i = returns.length-1; i >=0; i--){
       	$scope.myChart.data.labels.push( returns[i].date);
       	$scope.myChart.data.datasets[0].data.push( returns[i].return);
+      	$scope.myChart.data.datasets[1].data.push(realizedVol[i]);
+      	$scope.myChart.data.datasets[2].data.push(-1*realizedVol[i]);
+      	$scope.myChart.data.datasets[3].data.push(2*realizedVol[i]);
+      	$scope.myChart.data.datasets[4].data.push(-2*realizedVol[i]);
       	console.log('Pushing');
       	$scope.myChart.update();
       }
